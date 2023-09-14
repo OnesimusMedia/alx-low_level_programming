5d740e7723d5b2cc1c609426220684a208690e40
odindescent <servaas.steenkamp@gmail.com>
2023-09-14 12:36:22 -0700

    0-isupper added

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - more funtions check character is uppercase.
+ *
+ * @c: character for evaluation.
+ * Return: 1 upper or 0 lower.
+ *
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+	return (0);
+}
