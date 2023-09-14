93a5fcf87388fdc2bd28a1db759268c2c51ec059
odindescent <servaas.steenkamp@gmail.com>
2023-09-14 13:20:51 -0700

    trying

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Fizz Buzz interview
  *
@@ -10,25 +9,36 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i != 100)
 		{
-			printf("FizzBuzz");
-			printf(" ");
+			if (i % 3 == 0 && i % 5 == 0)
+			{
+				printf("FizzBuzz");
+				printf(" ");
+			}
+			else if (i % 3 == 0)
+			{
+				printf("Fizz");
+				printf(" ");
+
+			}
+			else if (i % 5 == 0)
+			{
+				printf("Buzz");
+				printf(" ");
+			}
+			else
+			{
+				printf("%d", i);
+				printf(" ");
+			}
 		}
-		else if ((i % 3) == 0)
-		{
-			printf("Fizz");
-			printf(" ");
-		}
-		else if ((i % 5) == 0)
+		else
 		{
 			printf("Buzz");
-			printf(" ");
 		}
-		printf("%d", i);
-		printf(" ");
 	}
 	printf("\n");
 	return (0);
