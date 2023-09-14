0f1ab6c42c9671121432b2e91142485e518c4645
odindescent <servaas.steenkamp@gmail.com>
2023-09-14 12:57:45 -0700

    4-print_most_numbers added

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - numbers void 0 to 9 ex. 2 and 4.
+ *
+ * Return: void.
+ */
+void print_most_numbers(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+		if (c != 2 && c != 4)
+		{
+			_putchar('0' + c);
+		}
+		c++;
+	}
+	_putchar('\n');
+}
