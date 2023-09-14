e07c0836c99189e22d2848a0af89092de3580525
odindescent <servaas.steenkamp@gmail.com>
2023-09-14 13:15:54 -0700

    not sure what the issue is

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,26 +12,23 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (((i % 3) == 0) && ((i % 5) == 0))
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
 			printf(" ");
 		}
-		else if (((i % 3) == 0))
+		else if ((i % 3) == 0)
 		{
 			printf("Fizz");
 			printf(" ");
 		}
-		else if (((i % 5) == 0))
+		else if ((i % 5) == 0)
 		{
 			printf("Buzz");
 			printf(" ");
 		}
-		else
-		{
-			printf("%d", i);
-			printf(" ");
-		}
+		printf("%d", i);
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
