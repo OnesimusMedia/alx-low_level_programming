2f65c2557e896bf4568e1a9bc631a222c234a183
odindescent <servaas.steenkamp@gmail.com>
2023-09-18 04:08:41 -0700

    2-strlen added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - the length of a string.
+ * @s: int variable
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	int conta = 0, i;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+		conta++;
+	}
+
+	return (conta);
+}
