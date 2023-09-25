3da1bb74641872ca888a7cae05def809530d6bfa
odindescent <servaas.steenkamp@gmail.com>
2023-09-24 22:06:45 -0700

    another try

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,8 +12,8 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 = a[(size + i) * i];
-		sum2 = a[(size - 1) * i];
+		sum1 = sum1 + a[(size + i) * i];
+		sum2 = sum2 + a[(size - 1) * i];
 	}
 
 	printf("%d, %d\n", sum1, sum2);
