a0b00c5f5cfc799ca4f467380e31cf248d9dff48
odindescent <servaas.steenkamp@gmail.com>
2023-09-29 20:55:24 -0700

    0-whatsmyname added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+		return (0);
+	}
+
+	return (1);
+}
