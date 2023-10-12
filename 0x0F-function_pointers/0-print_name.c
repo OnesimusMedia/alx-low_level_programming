8d3635ca90f569e1f3d67532e6d1bd764e788504
odindescent <servaas.steenkamp@gmail.com>
2023-10-12 09:46:43 -0700

    0-print_name added

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using a function pointer
+ * @name: name of the person
+ * @f: pointer to a function that prints a name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	    f(name);
+}
