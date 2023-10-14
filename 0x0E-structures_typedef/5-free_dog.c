5ae9e94660db9fc487f6a594b0af0aaa9a8038dc
odindescent <servaas.steenkamp@gmail.com>
2023-10-14 11:17:14 -0700

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Frees a dog structure
+ * @d: Pointer to the dog structure
+ *
+ * Description: This function frees the memory 
+ * allocated for the dog structure
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
