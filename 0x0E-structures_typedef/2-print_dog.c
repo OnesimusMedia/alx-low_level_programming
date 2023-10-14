48b52927f5eb3453d1578dd513206af67601ece5
odindescent <servaas.steenkamp@gmail.com>
2023-10-14 10:53:26 -0700

    print dog added

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stddef.h>
+
+/**
+ * print_dog - Prints information about a dog structure
+ * @d: Pointer to the dog structure
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+	}
+}
