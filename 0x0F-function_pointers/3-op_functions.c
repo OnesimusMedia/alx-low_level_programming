c5e0ab3ac3a631097beb05ba2701602d0879ab91
odindescent <servaas.steenkamp@gmail.com>
2023-10-14 23:03:58 -0700

    finally done

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,16 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Add two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/* Implement the other four functions (op_sub, op_mul, op_div, and op_mod) in a similar way. */
+
