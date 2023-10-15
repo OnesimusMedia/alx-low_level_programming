e6dac06dae751ad10be2115ddf30da69808e1626
odindescent <servaas.steenkamp@gmail.com>
2023-10-14 23:08:37 -0700

    fixed implementations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -12,5 +12,60 @@ int op_add(int a, int b)
 	return (a + b);
 }
 
-/* Implement the other four functions (op_sub, op_mul, op_div, and op_mod) in a similar way. */
+/**
+ * op_sub - Subtract two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Difference between a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
 
+/**
+ * op_mul - Multiply two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divide two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Result of a divided by b
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - Calculate the remainder of division
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Remainder of a divided by b
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
