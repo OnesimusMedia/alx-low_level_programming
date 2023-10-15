a013f7c5bc2d37dedc080b736238c34049c4dbc9
odindescent <servaas.steenkamp@gmail.com>
2023-10-14 23:25:36 -0700

    all errors removed

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,9 +2,19 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
+int (*get_op_func(char *s))(int, int);
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
+ *
+ * Return: 0 on success, 98 for wrong number of arguments, 99 for invalid operator,
+ * 100 for division/modulo by zero, and 1 for any other error
+ */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	int num1, num2;
 	int (*func_ptr)(int, int);
 
 	if (argc != 4)
@@ -17,7 +27,7 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 	func_ptr = get_op_func(argv[2]);
 
-	if (!func_ptr)
+	if (func_ptr == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
@@ -29,8 +39,7 @@ int main(int argc, char *argv[])
 		exit(100);
 	}
 
-	result = func_ptr(num1, num2);
-	printf("%d\n", result);
+	printf("%d\n", func_ptr(num1, num2));
 
 	return (0);
 }
